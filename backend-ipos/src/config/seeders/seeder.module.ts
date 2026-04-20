86c07fb008f27786280516042b52265187af42b8
Rohith <rohith@Rohiths-MacBook-Air.local>
2026-04-20 17:28:46 +0530

    changed seeder.module

--- backend-ipos/src/config/seeders/seeder.module.ts
+++ backend-ipos/src/config/seeders/seeder.module.ts
@@ -5,12 +5,13 @@ import { PermissionsSeeder } from './permissions.seeder';
 import { SeederService } from './seeder.service';
 import { RolePermission } from 'src/auth/roles/role-permissions.entity';
 import { Role } from 'src/auth/roles/roles.entity';
+import { RolesSeeder } from './roles.seeder';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Permission, Role, RolePermission]), // ✅ include all
   ],
-  providers: [PermissionsSeeder, SeederService],
+  providers: [PermissionsSeeder, SeederService, RolesSeeder],
   exports: [SeederService],
 })
 export class SeederModule {}
